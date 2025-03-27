76899b44fd4dd72ee4d6ce398314edc0b02aa2b5
suchmolin11 <suchmolin11@gmail.com>
2025-03-27 10:11:42 -0400

    1st commit, inicializando entorno

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-100">HOLA MUNDO</h1>
+    </>
+  );
+}
+
+export default App;
